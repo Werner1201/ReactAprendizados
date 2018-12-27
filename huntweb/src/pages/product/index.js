b7f32665364b794d1cdd357be24e68bc8a6d5736
Werner1201 <werner.romling@unigranrio.br>
2018-12-27 11:34:22 -0200

    Navegação Pro Detalhe

--- huntweb/src/pages/product/index.js
+++ huntweb/src/pages/product/index.js
@@ -3,13 +3,35 @@ import api from '../../services/api';
 import './estilo.css';
 
 export default class Product extends Component {
-    
+    state = {
+        product: {
+
+        }
+    };
+
+    async componentDidMount(){
+        //pega o id enviado pela Url 
+        const {id} = this.props.match.params;
+
+        const resposta = await api.get(`/products/${id}`);
+        //Guarda no State Product
+        this.setState({product:resposta.data});
+    }
+
     render(){
+        const {product} = this.state;
+
         return(
-            <div id="detalhesProduct">
-            <h1>Product</h1>
-            <a href="/">Voltar</a>
+            <div className="DetalProduto">
+            <h1>{product.title}</h1>
+            <p>{product.description}</p>
+
+            <p>
+                URL: <a href={product.url}>{product.url}</a>
+            </p>
+            <a href="/" className="volta">Voltar</a>
             </div>
+            
         )
     }
 }
